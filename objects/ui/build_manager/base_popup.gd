53e2666eb20896a0ef12895a61011ab082984853
Eduard Sheliemietiev <eduardchelemetjev@gmail.com>
2024-08-18 11:22:56 +0300

    Popup background and close popups with escape key

--- objects/ui/build_manager/base_popup.gd
+++ objects/ui/build_manager/base_popup.gd
@@ -5,3 +5,9 @@ signal closed(result: Dictionary)
 
 func _on_cancel_button_pressed() -> void:
 	closed.emit({})
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.keycode == KEY_ESCAPE:
+			closed.emit({})
+			accept_event()
