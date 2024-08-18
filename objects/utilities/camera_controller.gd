8a5a9ae9e59f5573853a275cc51aafa897e05173
Eduard Sheliemietiev <eduardchelemetjev@gmail.com>
2024-08-18 19:29:07 +0300

    Fix piston init scale

--- objects/utilities/camera_controller.gd
+++ objects/utilities/camera_controller.gd
@@ -8,7 +8,7 @@ var camera_drag_start_position: Vector2 = Vector2()
 
 var move_speed: float = 800
 
-var zoom_levels: Array[float] = [0.25, 0.5, 0.75, 1, 2, 3]
+var zoom_levels: Array[float] = [0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3]
 var zoom_level_index = 3
 
 func _unhandled_input(event: InputEvent) -> void:
