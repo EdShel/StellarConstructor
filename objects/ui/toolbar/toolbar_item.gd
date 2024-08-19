6165394d92198f9d5124db50e33bb8c32ef6ffbd
Eduard Sheliemietiev <eduardchelemetjev@gmail.com>
2024-08-19 21:18:26 +0300

    Background for toolbar items

--- objects/ui/toolbar/toolbar_item.gd
+++ objects/ui/toolbar/toolbar_item.gd
@@ -14,3 +14,13 @@ func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		SC.toolbar_item_pressed.emit(name)
 		accept_event()
+
+
+func _on_mouse_entered() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(%Background, "scale", Vector2(1.2, 1.2), 0.1)
+
+
+func _on_mouse_exited() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(%Background, "scale", Vector2(1, 1), 0.1)
